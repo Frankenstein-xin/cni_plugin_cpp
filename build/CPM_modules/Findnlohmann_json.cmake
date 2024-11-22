e59fcfa92493032be7bf3ac060c10df25d59b9df
leon.c <leon.c@bytedance.com>
2024-11-22 07:07:15 +0000

    CPM build

--- build/CPM_modules/Findnlohmann_json.cmake
+++
@@ -1,3 +0,0 @@
-include("/root/source/cni_plugin_cpp/build/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("NAME;nlohmann_json;GITHUB_REPOSITORY;nlohmann/json;VERSION;3.11.3")
-set(nlohmann_json_FOUND TRUE)
